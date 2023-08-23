53e966634a1c6409464722bdeb9a839f8ed6cd83
cyr-ius <cyr-ius@ipocus.net>
2023-08-23 12:26:14 +0200

    Up

--- custom_components/bbox2/config_flow.py
+++ custom_components/bbox2/config_flow.py
@@ -19,7 +19,7 @@ _LOGGER = logging.getLogger(__name__)
 STEP_USER_DATA_SCHEMA = vol.Schema(
     {
         vol.Required(CONF_HOST, default=BBOX_URL): str,
-        vol.Required(CONF_PASSWORD): str,
+        vol.Optional(CONF_PASSWORD): str,
     }
 )
 
