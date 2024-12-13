e11f96fce898b0e7d48020edcc2986ab80343477
cyr-ius <cyr-ius@ipocus.net>
2024-12-13 13:11:04 +0100

    User _async_setup

--- custom_components/bbox/coordinator.py
+++ custom_components/bbox/coordinator.py
@@ -8,11 +8,13 @@ from typing import Any
 
 from bboxpy import Bbox
 
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import CONF_HOST, CONF_PASSWORD, CONF_VERIFY_SSL
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.aiohttp_client import async_create_clientsession
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
 
-from .const import CONF_DEFAULTS, CONF_HOST, CONF_PASSWORD, CONF_USE_TLS, DOMAIN, CONF_REFRESH_RATE
+from .const import CONF_DEFAULTS, CONF_REFRESH_RATE, CONF_USE_TLS, DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -27,30 +29,33 @@ class BboxDataUpdateCoordinator(DataUpdateCoordinator):
     ) -> None:
         """Class to manage fetching data API."""
         super().__init__(
-            hass, _LOGGER, name=DOMAIN,
+            hass,
+            _LOGGER,
+            name=DOMAIN,
             update_interval=timedelta(
-                seconds=entry.data.get(CONF_REFRESH_RATE, CONF_DEFAULTS[CONF_REFRESH_RATE])
-            )
+                seconds=entry.data.get(
+                    CONF_REFRESH_RATE, CONF_DEFAULTS[CONF_REFRESH_RATE]
+                )
+            ),
         )
         self.entry = entry
 
-    async def connect(self):
-        """Start Bbox connection"""
+    async def _async_setup(self) -> None:
+        """Start Bbox connection."""
         self.bbox = Bbox(
             password=self.entry.data[CONF_PASSWORD],
             hostname=self.entry.data[CONF_HOST],
             session=async_create_clientsession(self.hass),
             use_tls=self.entry.data.get(CONF_USE_TLS, False),
+            verify_ssl=self.entry.data.get(CONF_VERIFY_SSL, False),
         )
 
-    async def update_configuration(self, hass, entry):
-        """Update configuration"""
+    async def update_configuration(self, entry: ConfigEntry) -> None:
+        """Update configuration."""
         self.entry = entry
-        await self.connect()
+        await self._async_setup()
 
-        self.update_interval = timedelta(
-            seconds=entry.data[CONF_REFRESH_RATE]
-        )
+        self.update_interval = timedelta(seconds=entry.data[CONF_REFRESH_RATE])
         _LOGGER.debug("Coordinator refresh interval updated (%s)", self.update_interval)
 
         _LOGGER.debug("Force update")
